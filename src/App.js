aa4d1115adc565dd6f9736d07b011c57d0c09f54
Tommy <x.dvotion.x@gmail.com>
2022-01-24 00:35:42 +0100

    Increment e Decrement counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [count, seCount] = useState(4);
+
+  function decremntCount() {
+    seCount((prevCount) => prevCount - 1);
+  }
+
+  function incrementCount() {
+    seCount((prevCount) => prevCount + 1);
+  }
+
   return (
     <div className='App'>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
+      <button onClick={decremntCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
