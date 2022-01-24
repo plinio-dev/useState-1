99e23b7dea94e2b99e77280a0fa2621ca7cd56bd
Tommy <x.dvotion.x@gmail.com>
2022-01-24 02:53:48 +0100

    Arrow function nel bottone

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(4);
 
-  function decremntCount() {
-    setCount((prevCount) => prevCount - 1);
-  }
+  // function decremntCount() {
+  //   setCount((prevCount) => prevCount - 1);
+  // }
 
-  function incrementCount() {
-    setCount((prevCount) => prevCount + 1);
-  }
+  // function incrementCount() {
+  //   setCount((prevCount) => prevCount + 1);
+  // }
 
   return (
     <div className='App'>
-      <button onClick={decremntCount}>-</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
       <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
